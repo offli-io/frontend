5bcf22b6fd76c3617b20024e9d5d56cedc7d0f7f
Zofcik <Martin_Zofcik@Dell.com>
2022-07-06 13:47:53 +0200

    Add registration-screen layout

--- offli/src/routes/routes.tsx
+++ offli/src/routes/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes as BaseRoutes } from 'react-router-dom'
 import LoginScreen from '../screens/login-screen'
+import RegistrationScreen from '../screens/registration-screen'
 
 const Routes = () => {
   return (
     <BaseRoutes>
       <Route path="/" element={<LoginScreen />} />
       <Route path="/login" element={<div>Login</div>} />
-      <Route path="/register" element={<div>Registracia</div>} />
+      <Route path="/register" element={<RegistrationScreen />} />
     </BaseRoutes>
   )
 }
