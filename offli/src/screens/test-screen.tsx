8c1de2e516b38c1914b8ecb9210ab416afa5b383
Adam Martiska <adam.martiska@kistler.com>
2022-11-28 00:05:24 +0100

    Add test screen and swipeable drawer component

---
+++ offli/src/screens/test-screen.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import MaximizeRoundedIcon from '@mui/icons-material/MaximizeRounded'
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
+import OffliButton from '../components/offli-button'
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'
+
+import image from '../assets/img/undraw_super_thank_you_blue.svg'
+
+const TestScreen = () => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography>This is testing screen for components</Typography>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={true}
+        onOpen={() => console.log('wtf')}
+        onClose={() => console.log('close')}
+      >
+        <Box sx={{ height: 200 }}>Drawer content</Box>
+      </SwipeableDrawer>
+    </Box>
+  )
+}
+
+export default TestScreen
