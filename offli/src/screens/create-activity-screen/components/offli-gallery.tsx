406e83c554e854ebd9537f30dfe9779596db29c3
Adam Martiska <adam.martiska@kistler.com>
2022-12-04 22:59:49 +0100

    Add loading progress

--- offli/src/screens/create-activity-screen/components/offli-gallery.tsx
+++ offli/src/screens/create-activity-screen/components/offli-gallery.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import TextField from '@mui/material/TextField'
 import Autocomplete, { AutocompleteProps } from '@mui/material/Autocomplete'
-import { Box, Chip, SxProps } from '@mui/material'
+import { Box, Chip, CircularProgress, SxProps } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import { getPredefinedPhotos } from '../../../api/activities/requests'
 
@@ -14,7 +14,7 @@ const OffliGallery: React.FC<ITimePickerProps> = ({
   tags,
   onPictureSelect,
 }) => {
-  const { data } = useQuery(['predefined-photos', tags], () =>
+  const { data, isLoading } = useQuery(['predefined-photos', tags], () =>
     getPredefinedPhotos(tags)
   )
   return (
@@ -22,29 +22,42 @@ const OffliGallery: React.FC<ITimePickerProps> = ({
       {tags?.map(tag => (
         <Chip label={tag} key={tag} sx={{ mx: 1, my: 0.5 }} color="primary" />
       ))}
-      <Box
-        sx={{
-          display: 'grid',
-          //   gridTemplateColumns:
-          //     'repeat(auto-fit, minmax(min(100%/3, max(64px, 100%/5)), 1fr))',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          my: 4,
-        }}
-      >
-        {data?.data?.pictures?.map(({ picture }) => (
-          <Box
-            sx={{
-              maxWidth: '100%',
-              m: 0.5,
-              boxShadow: '1px 3px 2px #ccc',
-            }}
-            onClick={() => picture && onPictureSelect(picture)}
-            key={picture}
-          >
-            <img src={picture} style={{ maxWidth: '100%' }} />
-          </Box>
-        ))}
-      </Box>
+      {isLoading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            mt: 4,
+          }}
+        >
+          <CircularProgress color="primary" />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: 'grid',
+            //   gridTemplateColumns:
+            //     'repeat(auto-fit, minmax(min(100%/3, max(64px, 100%/5)), 1fr))',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            my: 4,
+          }}
+        >
+          {data?.data?.pictures?.map(({ picture }) => (
+            <Box
+              sx={{
+                maxWidth: '100%',
+                m: 0.5,
+                boxShadow: '1px 3px 2px #ccc',
+              }}
+              onClick={() => picture && onPictureSelect(picture)}
+              key={picture}
+            >
+              <img src={picture} style={{ maxWidth: '100%' }} />
+            </Box>
+          ))}
+        </Box>
+      )}
     </Box>
   )
 }
