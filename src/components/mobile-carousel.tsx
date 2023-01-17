5d0fbd33ffbec5eaf227a1525df29b3ab305c041
Adam Martiska <adam.martiska@kistler.com>
2023-01-17 22:32:06 +0100

    Add horizontal carousel

---
+++ src/components/mobile-carousel.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import { Card, Typography, Box } from "@mui/material";
+
+interface ICarouselItem {
+  title?: string;
+  description?: string;
+  id?: string;
+  selected?: boolean;
+}
+
+interface IMobileCarouselProps {
+  items?: ICarouselItem[];
+  title?: string;
+  onItemSelect?: (id?: string) => void;
+}
+
+export const MobileCarousel: React.FC<IMobileCarouselProps> = ({
+  items = [],
+  title,
+  onItemSelect,
+}) => {
+  return (
+    <>
+      {title && (
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          {title}
+        </Typography>
+      )}
+      <Box
+        sx={{
+          display: "flex",
+          overflow: "auto",
+          scrollSnapType: "x mandatory",
+          "& > *": {
+            scrollSnapAlign: "center",
+          },
+          "::-webkit-scrollbar": { display: "none" },
+        }}
+      >
+        {items?.map((item) => (
+          <Card
+            raised={item?.selected}
+            key={item.title}
+            sx={{
+              minWidth: 100,
+              height: 50,
+              bgcolor: (theme) =>
+                item?.selected ? theme.palette.primary.main : "transparent",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            onClick={() => onItemSelect?.(item?.id)}
+          >
+            <Typography sx={{ color: item?.selected ? "white" : "black" }}>
+              {item.title}
+            </Typography>
+            {item?.description && (
+              <Typography
+                sx={{
+                  fontSize: "0.75rem",
+                  color: item?.selected ? "white" : "black",
+                }}
+              >
+                {item.description}
+              </Typography>
+            )}
+          </Card>
+        ))}
+      </Box>
+    </>
+  );
+};
