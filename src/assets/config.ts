fd9d7cd0d294d762abd98df930756712959e6ca4
Adam Martiska <adam.martiska@kistler.com>
2023-01-17 21:44:58 +0100

    Login formatting issues

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_DEV_URL = 'http://192.168.1.86:8080'
+export const DEFAULT_DEV_URL = "http://localhost:8080";
 
 // use ip address of your PC when testing on mobile device '192.168.1.86'
-export const DEFAULT_KEYCLOAK_URL = 'http://192.168.1.86:8082'
+export const DEFAULT_KEYCLOAK_URL = "http://192.168.1.86:8082";
--- src/screens/login-screen.tsx
+++ src/screens/login-screen.tsx
@@ -1,67 +1,67 @@
-import React from 'react'
+import React from "react";
 import {
   Box,
   TextField,
   Typography,
   InputAdornment,
   IconButton,
-} from '@mui/material'
-import qs from 'qs'
-import Logo from '../components/logo'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { Controller, useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-import OffliButton from '../components/offli-button'
-import LabeledDivider from '../components/labeled-divider'
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-import VisibilityIcon from '@mui/icons-material/Visibility'
-import axios from 'axios'
-import { useSnackbar } from 'notistack'
+} from "@mui/material";
+import qs from "qs";
+import Logo from "../components/logo";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import OffliButton from "../components/offli-button";
+import LabeledDivider from "../components/labeled-divider";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import axios from "axios";
+import { useSnackbar } from "notistack";
 import {
   getAuthToken,
   setAuthToken,
   setRefreshToken,
-} from '../utils/token.util'
-import { AuthenticationContext } from '../assets/theme/authentication-provider'
-import { ApplicationLocations } from '../types/common/applications-locations.dto'
-import { useNavigate } from 'react-router-dom'
-import { DEFAULT_KEYCLOAK_URL } from '../assets/config'
-import { loginUser } from '../api/auth/requests'
+} from "../utils/token.util";
+import { AuthenticationContext } from "../assets/theme/authentication-provider";
+import { ApplicationLocations } from "../types/common/applications-locations.dto";
+import { useNavigate } from "react-router-dom";
+import { DEFAULT_KEYCLOAK_URL } from "../assets/config";
+import { loginUser } from "../api/auth/requests";
 
 export interface FormValues {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 const schema: () => yup.SchemaOf<FormValues> = () =>
   yup.object({
     username: yup.string().defined().required(),
     password: yup.string().defined().required(),
-  })
+  });
 
 const LoginScreen: React.FC = () => {
-  const [showPassword, setShowPassword] = React.useState(false)
-  const { setUserInfo, setStateToken } = React.useContext(AuthenticationContext)
-  const { enqueueSnackbar } = useSnackbar()
-  const navigate = useNavigate()
+  const [showPassword, setShowPassword] = React.useState(false);
+  const { setUserInfo, setStateToken } = React.useContext(
+    AuthenticationContext
+  );
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
-  const handleClickShowPassword = () => setShowPassword(!showPassword)
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
 
   const { control, handleSubmit, watch } = useForm<FormValues>({
     defaultValues: {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     },
     resolver: yupResolver(schema()),
-    mode: 'onChange',
-  })
+    mode: "onChange",
+  });
 
   const { isLoading, mutate } = useMutation(
-    ['login'],
+    ["login"],
     (formValues: FormValues) => {
-      !!setUserInfo && setUserInfo({ username: formValues?.username })
-      localStorage.setItem('username', formValues?.username)
       //keycloak login that we are using no more
       // const data = {
       //   ...formValues,
@@ -74,46 +74,48 @@ const LoginScreen: React.FC = () => {
       //   data: qs.stringify(data),
       //   url: `${DEFAULT_KEYCLOAK_URL}/realms/Offli/protocol/openid-connect/token`,
       // }
-      return loginUser(formValues)
+      return loginUser(formValues);
     },
     {
-      onSuccess: data => {
-        console.log(data?.data)
+      onSuccess: (data, params) => {
+        console.log(data?.data);
         // setAuthToken(data?.data?.access_token)
         // setRefreshToken(data?.data?.refresh_token)
-        setStateToken(data?.data?.token?.access_token ?? null)
-        navigate(ApplicationLocations.ACTIVITIES)
+        setStateToken(data?.data?.token?.access_token ?? null);
+        !!setUserInfo && setUserInfo({ username: params?.username });
+        localStorage.setItem("username", params?.username);
+        navigate(ApplicationLocations.ACTIVITIES);
       },
-      onError: error => {
-        enqueueSnackbar('Failed to log in', { variant: 'error' })
+      onError: (error) => {
+        enqueueSnackbar("Failed to log in", { variant: "error" });
       },
     }
-  )
+  );
 
   const handleFormSubmit = React.useCallback(
     (values: FormValues) => mutate(values),
     [mutate]
-  )
+  );
 
   return (
     <form
       onSubmit={handleSubmit(handleFormSubmit)}
       style={{
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
       }}
       // className="backgroundImage"
     >
       <Logo />
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          alignItems: "center",
         }}
       >
         <Box id="signIn"></Box>
@@ -137,7 +139,7 @@ const LoginScreen: React.FC = () => {
               //   t(`value.${nextStep?.authenticationType}.placeholder`)
               // }
               // disabled={methodSelectionDisabled}
-              sx={{ width: '80%', mb: 2 }}
+              sx={{ width: "80%", mb: 2 }}
             />
           )}
         />
@@ -149,7 +151,7 @@ const LoginScreen: React.FC = () => {
               {...field}
               //label="Username"
               placeholder="Heslo"
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               // variant="filled"
               error={!!error}
               // helperText={
@@ -157,7 +159,7 @@ const LoginScreen: React.FC = () => {
               //   t(`value.${nextStep?.authenticationType}.placeholder`)
               // }
               //disabled={methodSelectionDisabled}
-              sx={{ width: '80%' }}
+              sx={{ width: "80%" }}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -178,21 +180,21 @@ const LoginScreen: React.FC = () => {
         <OffliButton variant="text" disabled={isLoading}>
           <Typography
             variant="subtitle2"
-            sx={{ fontWeight: 'bold', ml: '-70%' }}
+            sx={{ fontWeight: "bold", ml: "-70%" }}
           >
             Forgot your password?
           </Typography>
         </OffliButton>
       </Box>
       <OffliButton
-        sx={{ width: '80%', mb: 5 }}
+        sx={{ width: "80%", mb: 5 }}
         type="submit"
         isLoading={isLoading}
       >
         Login
       </OffliButton>
     </form>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
