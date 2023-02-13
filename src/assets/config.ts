37101d17b0aaedcbe6d7b0a5b6b2e2e031b094c1
Adam Martiska <adam.martiska@kistler.com>
2023-02-13 17:13:26 +0100

    Remove default url

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,6 +1,4 @@
-export const DEFAULT_DEV_URL =
-  process.env.REACT_APP_API_URL ??
-  "https://api-gateway-semjacko.cloud.okteto.net";
+export const DEFAULT_DEV_URL = process.env.REACT_APP_API_URL;
 //   "https://api-gateway-jurajpaska8.cloud.okteto.net";
 
 // use ip address of your PC when testing on mobile device '192.168.1.86'
