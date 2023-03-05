c8fa31b30d1ffad87d1a8c92f75140f83025fddd
MartinZofcikTakeda <martin.zofcik@takeda.com>
2023-03-05 18:18:05 +0100

    Add EditActivityScreen, without PUT to BE

--- src/screens/edit-activity-screen/edit-activity-screen.tsx
+++ src/screens/edit-activity-screen/edit-activity-screen.tsx
@@ -6,8 +6,9 @@ import {
   FormControlLabel,
   Select,
   MenuItem,
+  Chip,
 } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import BackHeader from "../../components/back-header";
 import { ICustomizedLocationStateDto } from "../../types/common/customized-location-state.dto";
@@ -23,10 +24,15 @@ import "dayjs/locale/sk";
 import { IOSSwitch } from "./components/ios-switch";
 import { ActivityVisibilityEnum } from "../../types/activities/activity-visibility-enum.dto";
 import {
+  ACTIVITY_FEE_OPTIONS,
   MAX_ACTIVITY_ATTENDANCE,
+  MAX_ACTIVITY_DESC_LENGTH,
   MIN_ACTIVITY_ATTENDANCE,
 } from "../../utils/activities-constants";
 import ActionButton from "../../components/action-button";
+import { useTags } from "../../hooks/use-tags";
+import { IPredefinedTagDto } from "../../types/activities/predefined-tag.dto";
+
 interface IProps {}
 
 interface IEditActivity {
@@ -37,7 +43,13 @@ interface IEditActivity {
   isPrivate: boolean | undefined;
   maxAttendance: number;
   price: string;
+  additionalDesc: string;
   // startTime: Dayjs | undefined;
+}
+
+interface ICategoryTag {
+  title: string;
+  active: boolean;
 }
 
 const schema: () => yup.SchemaOf<IEditActivity> = () =>
@@ -51,7 +63,11 @@ const schema: () => yup.SchemaOf<IEditActivity> = () =>
       .number()
       .defined()
       .required("Please enter How many people can attend"),
-    price: yup.string().defined().required("Please enter the Fee"),
+    price: yup.string().defined().required("Please enter the Price"),
+    additionalDesc: yup
+      .string()
+      .defined()
+      .required("Please enter the Description"),
     // about_me: yup.string().defined().required("Please enter your aboutMe"),
     // location: yup.string().defined().required("Please enter your location"),
     // birthdate: yup.date().nullable().required("Please enter your birthDate"),
@@ -66,11 +82,13 @@ const EditActivityScreen: React.FC<IProps> = () => {
   const location = useLocation();
   const from = (location?.state as ICustomizedLocationStateDto)?.from;
   const theme = useTheme();
+  const lodash = require("lodash"); // to create array within given range easily
 
+  const [categoryTags, setCategoryTags] = useState<ICategoryTag[] | null>(null); /// TODO dat prec | null
+  const [activeCategoryTags, setActiveCategoryTags] = useState<string[]>([]);
+
+  const { data: predefinedTags } = useTags();
   const { data: { data = {} } = {} }: any = useActivities({ id });
-
-  const lodash = require("lodash"); // to create array within range easily
-  console.log(data);
 
   const { control, handleSubmit, reset, formState } = useForm<IEditActivity>({
     defaultValues: {
@@ -81,10 +99,59 @@ const EditActivityScreen: React.FC<IProps> = () => {
       isPrivate: true,
       maxAttendance: MAX_ACTIVITY_ATTENDANCE,
       price: "",
+      additionalDesc: "",
     },
     resolver: yupResolver(schema()),
     mode: "onChange",
   });
+
+  useEffect(() => {
+    // if predefinedTag is included in activity tags, set tag`s active param to true and vice versa,
+    // also add active tags to activeCategoryTags, for easier insert of tags into PUT request
+
+    let tagsTmp: ICategoryTag[] = [];
+    let activeTagsTmp: string[] = [];
+    if (predefinedTags?.data?.tags && predefinedTags?.data?.tags?.length > 0) {
+      if (data?.activity?.tags && data?.activity?.tags?.length > 0) {
+        predefinedTags?.data?.tags.map((tag) => {
+          if (data?.activity?.tags?.includes(tag?.title)) {
+            tagsTmp.push({ title: tag?.title, active: true });
+            activeTagsTmp.push(tag?.title);
+          }
+          if (!data?.activity?.tags?.includes(tag?.title)) {
+            tagsTmp.push({ title: tag?.title, active: false });
+          }
+          // else {
+          //   return;
+          // }
+        });
+        setCategoryTags(tagsTmp);
+        setActiveCategoryTags(activeTagsTmp);
+      }
+    }
+  }, [predefinedTags, data]);
+
+  const handleChipClick = (index: number, title: string) => {
+    if (categoryTags) {
+      var newTagsArr = [...categoryTags];
+
+      newTagsArr[index].active = !newTagsArr[index].active;
+      setCategoryTags(newTagsArr);
+    }
+
+    if (activeCategoryTags) {
+      var newActiveCategoryTags = [...activeCategoryTags];
+      if (activeCategoryTags.includes(title)) {
+        var newActiveCategoryTags = activeCategoryTags.filter(function (e) {
+          return e !== title;
+        });
+      }
+      if (!activeCategoryTags.includes(title)) {
+        newActiveCategoryTags.push(title);
+      }
+      setActiveCategoryTags(newActiveCategoryTags);
+    }
+  };
 
   useEffect(() => {
     const recievedStartDateTime = new Date(data?.activity?.datetime_from);
@@ -109,6 +176,7 @@ const EditActivityScreen: React.FC<IProps> = () => {
       isPrivate: isPrivateA,
       maxAttendance: data?.activity?.limit,
       price: data?.activity?.price,
+      additionalDesc: data?.activity?.description,
     });
   }, [data]);
 
@@ -118,6 +186,7 @@ const EditActivityScreen: React.FC<IProps> = () => {
     // navigate(ApplicationLocations.PROFILE)
     //sendUpdateProfile(values);
     console.log(values);
+    console.log(categoryTags);
   }, []);
 
   return (
@@ -314,10 +383,10 @@ const EditActivityScreen: React.FC<IProps> = () => {
                 <TextField
                   select
                   {...field}
-                  label="Attendance Limit"
+                  label="How many people can attend?"
                   variant="outlined"
                   error={!!error}
-                  // helperText={error?.message}
+                  helperText={error?.message}
                   //disabled={methodSelectionDisabled}
                   sx={{ width: "95%", mt: 3 }}
                 >
@@ -336,7 +405,6 @@ const EditActivityScreen: React.FC<IProps> = () => {
               )}
             />
             {/* ///////////////////////////////////////////////////////////////////////////////// PRICE */}
-
             <Controller
               name="price"
               control={control}
@@ -344,33 +412,73 @@ const EditActivityScreen: React.FC<IProps> = () => {
                 <TextField
                   select
                   {...field}
-                  label="Attendance Limit"
+                  label="Any fees?"
                   variant="outlined"
                   error={!!error}
-                  // helperText={error?.message}
+                  helperText={error?.message}
                   //disabled={methodSelectionDisabled}
                   sx={{ width: "95%", mt: 3 }}
                 >
-                  {lodash
-                    .range(
-                      MIN_ACTIVITY_ATTENDANCE,
-                      MAX_ACTIVITY_ATTENDANCE + 1,
-                      1
-                    )
-                    .map((item: number) => (
-                      <MenuItem key={item} value={item}>
-                        {item}
-                      </MenuItem>
-                    ))}
+                  {ACTIVITY_FEE_OPTIONS.map((item: string) => (
+                    <MenuItem key={item} value={item}>
+                      {item}
+                    </MenuItem>
+                  ))}
                 </TextField>
               )}
             />
+            {/* ///////////////////////////////////////////////////////////////////////////////// ADDITIONAL DESC */}
+            <Controller
+              name="additionalDesc"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  multiline
+                  label="Additional description"
+                  variant="outlined"
+                  error={!!error}
+                  helperText={error?.message}
+                  inputProps={{ maxLength: MAX_ACTIVITY_DESC_LENGTH }}
+                  //disabled={methodSelectionDisabled}
+                  sx={{ width: "95%", mt: 3 }}
+                />
+              )}
+            />
+            {/* ///////////////////////////////////////////////////////////////////////////////// CATEGORY / TAGS */}
+            {categoryTags && categoryTags.length > 0 && (
+              <>
+                <Typography variant="h6" sx={{ width: "100%", mt: 2 }}>
+                  <b>Category</b>
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    flexWrap: "wrap",
+                    mt: 0.5,
+                  }}
+                >
+                  {categoryTags.map((tag: ICategoryTag, index) => (
+                    <Chip
+                      label={tag?.title}
+                      key={tag?.title}
+                      sx={{ mr: 0.2, borderRadius: "8px" }}
+                      color="primary"
+                      variant={tag?.active ? "filled" : "outlined"}
+                      onClick={() => handleChipClick(index, tag.title)}
+                    />
+                  ))}
+                </Box>
+              </>
+            )}
 
-            {/* </Box> */}
+            {/* ///////////////////////////////////////////////////////////////////////////////// SUBMIT BTN */}
             <ActionButton
               type="submit"
               text="Save"
-              // sx={{ width: "100% !important" }}
+              sx={{ mt: 4 }}
               disabled={!formState.isValid}
             />
           </form>
