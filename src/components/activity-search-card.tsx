0f427187c7049c3d8c3a98d0867cf1d63efc31d7
Adam Martiska <adam.martiska@kistler.com>
2023-03-30 00:22:17 +0200

    Add join activity button and activity tags

--- src/components/activity-search-card.tsx
+++ src/components/activity-search-card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Card, SxProps, Typography } from "@mui/material";
+import { Box, Card, IconButton, SxProps, Typography } from "@mui/material";
 import { IActivity } from "../types/activities/activity.dto";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import LockIcon from "@mui/icons-material/Lock";
@@ -15,7 +15,9 @@ import activityPlaceholderImage from "../assets/img/activity-placeholder-image.s
 import { calculateDistance } from "../utils/calculate-distance.util";
 import { useUsers } from "../hooks/use-users";
 import { AuthenticationContext } from "../assets/theme/authentication-provider";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import { LatLngTuple } from "leaflet";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 interface IMyActivityCardProps {
   activity?: IActivity;
@@ -45,9 +47,7 @@ const ActivitySearchCard: React.FC<IMyActivityCardProps> = ({
         boxSizing: "border-box",
         boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
         borderRadius: 1,
-        px: 2,
         mb: 2,
-        mr: 2,
         ...sx,
       }}
       onClick={() => onPress(activity)}
@@ -67,29 +67,48 @@ const ActivitySearchCard: React.FC<IMyActivityCardProps> = ({
           width: "100%",
         }}
       >
-        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-          {activity?.title}
-        </Typography>
-        <Typography sx={{ fontSize: 12 }}>
-          {activity?.location?.name}
-        </Typography>
-
-        <Typography sx={{ fontSize: 12 }}>
-          {format(
-            (activity?.datetime_from
-              ? new Date(activity?.datetime_from)
-              : new Date()) as Date,
-            "dd:mm:yyyy , hh:mm"
-          )}
-        </Typography>
         <Box sx={{ display: "flex" }}>
+          <Box sx={{ width: "80%" }}>
+            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+              {activity?.title}
+            </Typography>
+            <Typography sx={{ fontSize: 12 }}>
+              {activity?.location?.name}
+            </Typography>
+
+            <Typography sx={{ fontSize: 12 }}>
+              {format(
+                (activity?.datetime_from
+                  ? new Date(activity?.datetime_from)
+                  : new Date()) as Date,
+                "dd:mm:yyyy , hh:mm"
+              )}
+            </Typography>
+          </Box>
+          <IconButton onClick={() => console.log("join activity")}>
+            <AddCircleOutlineIcon color="primary" />
+          </IconButton>
+        </Box>
+        <Box sx={{ display: "flex", gap: 2, mt: 1 }}>
           <Box sx={{ display: "flex", alignItems: "center" }}>
-            <NearMeIcon sx={{ fontSize: 18, mr: 1 }} />
-            <Typography>
+            <NearMeIcon sx={{ fontSize: 18, mr: 0.5 }} />
+            <Typography sx={{ fontSize: 14 }}>
               {`${calculateDistance(activity?.location?.coordinates, {
                 lon: 25.2,
                 lat: -75.25,
               })} km`}
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <PeopleAltIcon sx={{ fontSize: 18, mr: 0.5 }} />
+            <Typography sx={{ fontSize: 14 }}>
+              {`${activity?.participants?.length ?? 0}/${activity?.limit}`}
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <MonetizationOnIcon sx={{ fontSize: 18, mr: 0.5 }} />
+            <Typography sx={{ fontSize: 14 }}>
+              {`${activity?.price}`}
             </Typography>
           </Box>
         </Box>
