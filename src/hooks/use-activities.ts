4b5d5ee158b8f521877b417654d8fc1124a63946
Adam Martiska <adam.martiska@kistler.com>
2023-08-10 15:04:20 +0200

    Map fixes

--- src/hooks/use-activities.ts
+++ src/hooks/use-activities.ts
@@ -1,13 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
 import { useSnackbar } from "notistack";
 import { getActivity } from "../api/activities/requests";
+import { AxiosResponse } from "axios";
+import { IActivityRestDto } from "../types/activities/activity-rest.dto";
+import { IActivityListRestDto } from "../types/activities/activity-list-rest.dto";
 
-export const useActivities = <T>({
-  id,
-  text,
-  tag,
-  date,
-}: { id?: number; text?: string; tag?: string[]; date?: Date | null } = {}) => {
+export const useActivities = <T>(
+  {
+    id,
+    text,
+    tag,
+    date,
+  }: { id?: number; text?: string; tag?: string[]; date?: Date | null } = {},
+  queryOptions?: Omit<UseQueryOptions, "queryKey" | "queryFn" | "initialData">
+) => {
   const { enqueueSnackbar } = useSnackbar();
   const { data, isLoading } = useQuery(
     ["activities", id, text, tag, date],
@@ -22,6 +28,7 @@ export const useActivities = <T>({
       refetchOnMount: true,
       refetchOnWindowFocus: true,
       refetchOnReconnect: true,
+      ...(queryOptions as any),
     }
   );
 
