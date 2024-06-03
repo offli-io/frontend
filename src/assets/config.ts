960d5bd8f3a0b17ffb12fdab382a8deb3029cf99
Adam Martiska <adam.martiska@kistler.com>
2024-06-03 13:24:51 +0200

    Change backend url to azure and prevent user scaling on web app (zooming in and out)

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_DEV_URL = 'https://app.offli.eu/api';
+export const DEFAULT_DEV_URL = 'https://offli-gateway.prod.azure.offli.eu/api';
 
 // use ip address of your PC when testing on mobile device '192.168.1.86'
